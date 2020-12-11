708858470c1a00ec40becb53fb6b0df191d2b38d
Alice <marja1895@gmail.com>
2020-12-12 01:39:19 +0200

    fetching data from api

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,54 +1,26 @@
 import React from "react"
 import classes from "./Users.module.css"
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.jpg';
 
 let Users = (props) => {
-	if (props.users.length === 0) {
-		props.setUsers([
-			{
-				id: 1,
-				photoUrl: 'https://i.pinimg.com/736x/c1/c5/c9/c1c5c948b57be809e7037d47d98b62b6.jpg',
-				followed: true,
-				fullName: 'Dmitry',
-				status: 'I am a boss',
-				location:
-				{
-					city: 'Minsk',
-					country: 'Belarus'
-				}
-			},
-			{
-				id: 2,
-				photoUrl: 'https://i.pinimg.com/736x/c1/c5/c9/c1c5c948b57be809e7037d47d98b62b6.jpg',
-				followed: true,
-				fullName: 'Andrey',
-				status: 'I am cool developer',
-				location:
-				{
-					city: 'Ottava',
-					country: 'Canada'
-				}
-			},
-			{
-				id: 3,
-				photoUrl: 'https://i.pinimg.com/736x/c1/c5/c9/c1c5c948b57be809e7037d47d98b62b6.jpg',
-				followed: true,
-				fullName: 'Alice',
-				status: 'I am working at SpaceX',
-				location:
-				{
-					city: 'Los-Angeles',
-					country: 'USA'
-				}
-			}])
+	let getUsers = () => {
+		if (props.users.length === 0) {
+			axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+				props.setUsers(response.data.items);
+			})
+
+		}
 	}
 	return (
 		<div>
+			<button onClick={getUsers}>Get users</button>
 			{props.users.map((u) => (
 				<div key={u.id}>
 					<span>
 						<div>
 
-							<img src={u.photoUrl} className={classes.userPhoto} />
+							<img src={u.photos.small != null ? u.photos.small : userPhoto} className={classes.userPhoto} />
 						</div>
 						<div>
 							{u.followed
@@ -58,12 +30,12 @@ let Users = (props) => {
 						</div>
 					</span>
 					<span>
-						<div> {u.fullName}</div>
+						<div> {u.name}</div>
 						<div>{u.status}</div>
 					</span>
 					<span>
-						<div>{u.location.country}</div>
-						<div>{u.location.city}</div>
+						<div>{"u.location.country"}</div>
+						<div>{"u.location.city"}</div>
 					</span>
 				</div>
 			))}
